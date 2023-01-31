970d523a21a6d02e314d4de6da0da6b0c4fda75c
acconde <114787853+acconde@users.noreply.github.com>
2023-02-01 05:40:40 +0800

    URL

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,4 +1,4 @@
 export default {
-  BASE_URL: `https://capstone-be1p.vercel.app//api/v1`,
+  BASE_URL: `https://capstone-be1p.vercel.app:60000/api/v1`,
   AWS_URL: 'https://mewmewxx.s3.ap-northeast-1.amazonaws.com/'
 }
--- server/app.js
+++ server/app.js
@@ -9,8 +9,6 @@ const socket = require('./config/socket');
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
