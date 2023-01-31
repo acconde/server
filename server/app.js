2562934c1dde89257de3e46b0ebfb2d4a3634d4f
acconde <114787853+acconde@users.noreply.github.com>
2023-02-01 07:01:44 +0800

    jade-pug

--- server/app.js
+++ server/app.js
@@ -9,6 +9,8 @@ const socket = require('./config/socket');
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
